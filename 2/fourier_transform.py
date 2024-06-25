de01d5672303dc847fbe3e8e030746ca158c901c
Yuval <yuvtaub1308@gmail.com>
2024-06-26 01:56:31 +0300

    fixed fourier transform

--- 2/fourier_transform.py
+++ 2/fourier_transform.py
@@ -5,22 +5,22 @@ import matplotlib.pyplot as plt
 
 def fourier_transform(img):
     f = np.fft.fft2(img)
-    fshift = np.fft.fftshift(f)  # shift the zero frequency component to the center
-    amplitude = np.abs(fshift)
+    fshift = np.fft.fftshift(f)
+    amplitude = 20 * np.log(np.abs(fshift))
     phase = np.angle(fshift)
 
     return f, amplitude, phase
 
 def plot_fourier_transform(title, amplitude, phase, fig_num):
     plt.figure(fig_num, figsize=(15, 5))
-    plt.subplot(131), plt.imshow(amplitude, cmap='gray')
+    plt.subplot(121), plt.imshow(amplitude, cmap='gray')
     plt.title(title + ' Amplitude Spectrum'), plt.xticks([]), plt.yticks([])
 
     # log scale
-    plt.subplot(132), plt.imshow(np.log(1 + amplitude), cmap='gray')
-    plt.title(title + ' Log Amplitude Spectrum'), plt.xticks([]), plt.yticks([])
+    # plt.subplot(132), plt.imshow(np.log(1 + amplitude), cmap='gray')
+    # plt.title(title + ' Log Amplitude Spectrum'), plt.xticks([]), plt.yticks([])
 
-    plt.subplot(133), plt.imshow(phase, cmap='gray')
+    plt.subplot(122), plt.imshow(phase, cmap='gray')
     plt.title(title + ' Phase Spectrum'), plt.xticks([]), plt.yticks([])
 
 def resize_image(img, shape):
@@ -47,46 +47,49 @@ def main():
 
     # plot amplitude difference
     plt.figure(3, figsize=(10, 5))
-    plt.subplot(121), plt.imshow(amplitude_diff, cmap='gray')
+    plt.subplot(111), plt.imshow(amplitude_diff, cmap='gray')
     plt.title('Amplitude Difference'), plt.xticks([]), plt.yticks([])
 
     # log scale
-    plt.subplot(122), plt.imshow(np.log(1 + amplitude), cmap='gray')
-    plt.title('Log Amplitude Spectrum'), plt.xticks([]), plt.yticks([])
+    # plt.subplot(122), plt.imshow(np.log(1 + amplitude), cmap='gray')
+    # plt.title('Log Amplitude Spectrum'), plt.xticks([]), plt.yticks([])
 
 
     # c) Compute the Fourier Transform of the images chita.jpeg and zebra.jpeg and plot the amplitude and phase spectrum of them respectively
     # open chita.jpeg and zebra.jpeg
-    img_chita = cv2.imread('../images/chita.jpeg', cv2.IMREAD_GRAYSCALE)
+    img_cheetah = cv2.imread('../images/chita.jpeg', cv2.IMREAD_GRAYSCALE)
     img_zebra = cv2.imread('../images/zebra.jpeg', cv2.IMREAD_GRAYSCALE)
 
-    # resize images to the same shape
-    target_shape = (256, 256)
-    img_chita_resized = resize_image(img_chita, target_shape)
-    img_zebra_resized = resize_image(img_zebra, target_shape)
-
     # compute the Fourier Transform of the images
-    f_chita, amplitude_chita, phase_chita = fourier_transform(img_chita_resized)
-    f_zebra, amplitude_zebra, phase_zebra = fourier_transform(img_zebra_resized)
+    f_cheetah, amplitude_cheetah, phase_cheetah = fourier_transform(img_cheetah)
+    f_zebra, amplitude_zebra, phase_zebra = fourier_transform(img_zebra)
 
     # plot the amplitude of chita.jpeg
     plt.figure(4, figsize=(15, 5))
-    plt.subplot(121), plt.imshow(amplitude_chita, cmap='gray')
+    plt.subplot(121), plt.imshow(amplitude_cheetah, cmap='gray')
     plt.title('chita.jpeg - Amplitude Spectrum'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122), plt.imshow(np.log(1 + amplitude_chita), cmap='gray')
-    plt.title('chita.jpeg - Log Amplitude Spectrum'), plt.xticks([]), plt.yticks([])
 
     # plot the phase of zebra.jpeg
-    plt.figure(5, figsize=(15, 5))
-    plt.subplot(111), plt.imshow(phase_zebra, cmap='gray')
+    plt.subplot(122), plt.imshow(phase_zebra, cmap='gray')
     plt.title('zebra.jpeg - Phase Spectrum'), plt.xticks([]), plt.yticks([])
 
 
     # d) reconstuct a new image using the amplitude of chita.jpeg and the phase of zebra.jpeg and plot the new image
-    # reconstruct the image
-    f_new = amplitude_chita * np.exp(1j * phase_zebra)
-    img_new = np.fft.ifft2(np.fft.ifftshift(f_new)).real
+    # resize the images to the same shape
+    img_zebra_resized = cv2.resize(img_zebra, (225, 225), interpolation=cv2.INTER_AREA)
+    img_cheetah_resized = cv2.resize(img_cheetah, (225, 225), interpolation=cv2.INTER_AREA)
 
+    # compute the Fourier Transform of the resized images
+    f_zebra_resized, amplitude_zebra_resized, phase_zebra_resized = fourier_transform(img_zebra_resized)
+    f_cheetah_resized, amplitude_cheetah_resized, phase_cheetah_resized = fourier_transform(img_cheetah_resized)
+
+    # combine the amplitude of the cheetah with the phase of the zebra
+    combined_spectrum = np.abs(np.fft.fftshift(f_cheetah_resized)) * np.exp(1j * phase_zebra_resized)
+    
+    # inverse FFT to get the reconstructed image
+    combined_ifft_resized = np.fft.ifftshift(combined_spectrum)
+    img_new = np.abs(np.fft.ifft2(combined_ifft_resized))
+    
     # plot the new image
     plt.figure(6, figsize=(10, 5))
     plt.imshow(img_new, cmap='gray')
